5d63b0cc6faabe0525d47eab6a6e2ee524ed6258
ThuktenSingye <thuktensingye2163@gmail.com>
2024-01-23 12:25:37 +0600

    testing

--- src/main/java/com/sdu/usermanagement/service/UserService.java
+++ src/main/java/com/sdu/usermanagement/service/UserService.java
@@ -27,7 +27,6 @@ import com.sdu.usermanagement.repository.GenderRepository;
 import com.sdu.usermanagement.repository.RoleRepository;
 import com.sdu.usermanagement.repository.SectionRepository;
 import com.sdu.usermanagement.repository.UserRepository;
-import com.sdu.usermanagement.repository.UserRoleRepository;
 import com.sdu.usermanagement.utility.FileNameGenerator;
 
 import jakarta.transaction.Transactional;
@@ -53,8 +52,6 @@ public class UserService implements  UserDetailsService {
     @Autowired
     private FileNameGenerator fileNameGenerator;
 
-    @Autowired
-    private UserRoleRepository userRoleRepository;
 
     @Autowired
     private LogService logService;
@@ -204,7 +201,7 @@ public class UserService implements  UserDetailsService {
                 return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
             }
             logService.logApplicationStatus("User deleted");
-            userRoleRepository.deleteByUserId(user_id);
+
             userRepository.deleteById(user_id);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
