a60356663dba96f61487fa2e05f004222baba5dd
ThuktenSingye <thuktensingye2163@gmail.com>
2024-01-23 12:20:21 +0600

    testing

--- src/main/java/com/sdu/usermanagement/repository/UserRoleRepository.java
+++ src/main/java/com/sdu/usermanagement/repository/UserRoleRepository.java
@@ -1,8 +1,7 @@
 package com.sdu.usermanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
 import com.sdu.usermanagement.model.UserRole;
@@ -10,6 +9,6 @@ import com.sdu.usermanagement.model.UserRole;
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
 
-    void deleteByUserId(@Param("userId") Integer userId);
+    void deleteByUserId(Integer userId);
     
 }
